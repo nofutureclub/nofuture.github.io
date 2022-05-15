46f5548f7383f03aa882e64d89f64d800f9d1205
Chris Blanquera <chris@incept.asia>
2022-05-15 12:55:09 +0800

    no code db form

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -26,6 +26,7 @@
 
   const holder = async function(session) {
     toggle('.holder', '.nonholder', '.holder-init')
+    name.value = state.account
   }
 
   const nonholder = async function(session) {
@@ -64,8 +65,83 @@
         notify('success', 'Wallet disconnected')
       }
       toggle('.disconnected', '.connected', '.connection-init')
+      name.value = ''
     }
   }
+
+  const toElement = function(html) {
+    const template = document.createElement('template');
+    template.innerHTML = html.trim();
+    return template.content.firstChild;
+  }
+
+  const toRelative = function(date) {
+    
+    const diff = Math.floor((Date.now() - date.getTime()) / 1000)
+    if (diff < 60) {
+      return `${diff}s ago`
+    } else if (diff < (60 * 60)) {
+      return `${Math.ceil(diff/60)}m ago`
+    } else if (diff < (60 * 60 * 24)) {
+      return `${Math.ceil(diff/(60 * 60))}h ago`
+    } else if (diff < (60 * 60 * 24 * 7)) {
+      return `${Math.ceil(diff/(60 * 60 * 24))}d ago`
+    } else if (diff < (60 * 60 * 24 * 7 * 4)) {
+      return `${Math.ceil(diff/(60 * 60 * 24 * 7))}w ago`
+    }
+
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    return `${months[date.getMonth()]} ${date.getDate()}`
+  }
+
+  const loadAnswers = async function() {
+    const response = await fetch(
+      [
+        'https://sheets.googleapis.com/v4/spreadsheets/',
+        '16NaNlYArZ6v8COt8ncYZADLpfqNon4nQpThel-T5mSA/values/',
+        '1?alt=json&key=AIzaSyAp5gMqvuRoIfBYXdWLyKhw1mRatpzqva4'
+      ].join('')
+    )
+    const container = document.querySelector('div.answers')
+    container.innerHTML = '<div class="loading">Loading Answers...</div>'
+    const json = await response.json()
+    json.values.shift()
+    container.innerHTML = ''
+    
+    for (const row of json.values) {
+      if (!row[0]?.length || !row[1]?.length || !row[2]?.length || row[3] === 'hide') {
+        continue;
+      }
+
+      const answer = toElement(tplAnswer
+        .replace('{AUTHOR}', row[1].indexOf('0x') !== 0 ? row[1]: [
+            row[1].substr(0, 6),
+            row[1].substr(row[1].length - 4)
+          ].join('...')
+        )
+        .replace('{DATE}', toRelative(new Date(row[0])))
+        .replace('{MESSAGE}', row[2])
+      )
+
+      container.appendChild(answer)
+    }
+  }
+
+  //------------------------------------------------------------------//
+  // Properties
+  const nft = blockapi.contract('nft')
+  const state = { connected: false }
+
+  const form = document.getElementById('google-form')
+  const frame = document.getElementById('google-frame')
+  const submit = form.querySelector('form button')
+  const name = document.getElementById('input-address')
+  const message = form.querySelector('form textarea')
+  const tplAnswer = document.getElementById('tpl-answer').innerHTML
+
+  let loaded = false
+  let submitted = false
 
   //------------------------------------------------------------------//
   // Events
@@ -78,13 +154,30 @@
     disconnected()
   })
 
-  //------------------------------------------------------------------//
-  // Properties
-  const nft = blockapi.contract('nft')
-  const state = { connected: false }
+  message.addEventListener('keyup', () => setTimeout(() => {
+    submit.disabled = true
+    if (!submitted && name.value.length && message.value.length) {
+      submit.disabled = false
+    }
+  }, 0))
+
+  form.addEventListener('submit', () => {
+    submitted = true
+    submit.disabled = true
+    submit.innerText = 'Working...'
+  })
+  
+  frame.addEventListener('load', () => {
+    if (loaded++ > 0) {
+      form.parentElement.removeChild(form)
+      notify('success', 'Message Sent!')
+      loadAnswers()
+    }
+  })
 
   //------------------------------------------------------------------//
   // Initialize
   window.doon('body')
   blockapi.startSession(blockmetadata, connected, disconnected, true)
+  loadAnswers()
 })()
